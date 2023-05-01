4cad0dbc28c65eea269452819aa7d15de4e0cfe3
Pavel Vasiliev <vas-pav@mail.ru>
2023-05-01 23:02:12 +0300

    выполнить 5-й шаг

--- app/src/main/java/hexlet/code/schemes/MapSchema.java
+++ app/src/main/java/hexlet/code/schemes/MapSchema.java
@@ -1,5 +1,6 @@
 package hexlet.code.schemes;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Map;
 
@@ -41,27 +42,27 @@ public class MapSchema extends BaseSchema {
             isValid = map.size() == size;
         }
     }
+
+    public MapSchema shape(Map<String, BaseSchema> schemes) throws NoSuchMethodException {
+        Method method = MapSchema.class.getDeclaredMethod("shapeLogic");
+        restrictions.put(method, schemes);
+        return this;
+    }
+
+    void shapeLogic() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = MapSchema.class.getDeclaredMethod("shapeLogic");
+        Map<String, BaseSchema> schemes = (Map<String, BaseSchema>) restrictions.get(method);
+        Map map = (Map) data;
+
+        for (Map.Entry<String, BaseSchema> schema : schemes.entrySet()) {
+            Object key = schema.getKey();
+            BaseSchema value = schema.getValue();
+            if (map.containsKey(key)) {
+                isValid = value.isValid(map.get(key));
+            }
+            if (!isValid) {
+                return;
+            }
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- app/src/test/java/hexlet/code/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/MapSchemaTest.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import hexlet.code.schemes.BaseSchema;
 import hexlet.code.schemes.MapSchema;
 import org.junit.jupiter.api.Test;
 import java.lang.reflect.InvocationTargetException;
@@ -34,6 +35,46 @@ public class MapSchemaTest {
 
         data.put("key2", "value2");
         assertThat(schema.isValid(data)).isTrue();
+    }
+
+    @Test
+    public void testShapeOfMapSchema() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Validator validator = new Validator();
+        MapSchema schema = validator.map();
+
+        Map<String, BaseSchema> schemas = new HashMap<>();
+        schemas.put("name", validator.string().required());
+        schemas.put("age", validator.number().positive());
+        schema.shape(schemas);
+
+        Map<String, Object> human1 = new HashMap<>();
+        human1.put("name", "Kolya");
+        human1.put("age", 100);
+        assertThat(schema.isValid(human1)).isTrue();
+
+        Map<String, Object> human2 = new HashMap<>();
+        human2.put("name", "Maya");
+        human2.put("age", null);
+        assertThat(schema.isValid(human2)).isTrue();
+
+        Map<String, Object> human3 = new HashMap<>();
+        human3.put("name", "");
+        human3.put("age", null);
+        assertThat(schema.isValid(human3)).isFalse();
+
+        Map<String, Object> human4 = new HashMap<>();
+        human4.put("name", "Vallya");
+        human4.put("age", -5);
+        assertThat(schema.isValid(human4)).isFalse();
+
+        Map<String, BaseSchema> schemas2 = new HashMap<>();
+        schemas2.put("name", validator.string().required().contains("lya").minLength(6));
+        schemas2.put("age", validator.number());
+        schema.shape(schemas2);
+
+        assertThat(schema.isValid(human1)).isFalse();
+        assertThat(schema.isValid(human2)).isFalse();
+        assertThat(schema.isValid(human4)).isTrue();
     }
 }
 
